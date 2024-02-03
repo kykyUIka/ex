398e70a2e649c54457ce168e31726ff18b47cf67
unknown <Student@305-7.spobip-bel.ru>
2024-02-03 14:11:19 +0300

    initial

---
+++ src/main/java/com/example/demoexnikolaev/DemoExNikolaevApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoexnikolaev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExNikolaevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoExNikolaevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demoexnikolaev/DemoExNikolaevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoexnikolaev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExNikolaevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
